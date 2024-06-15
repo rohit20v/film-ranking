b5fb1b9e29a66c140b9d9ba1252eb23885ceaa5e
Rohit <rohit.verma@itsrizzoli.it>
2024-06-15 21:14:44 +0200

    Updated Styling

--- app/root.tsx
+++ app/root.tsx
@@ -1,13 +1,18 @@
 import {Links, Meta, Outlet, Scripts, ScrollRestoration,} from "@remix-run/react";
-import "./index.css";
+import "./styles/index.css";
 import Navbar from "~/components/Navbar";
 
+
+export const links = () => [
+    { rel: "stylesheet", href: "../../node_modules/@picocss/pico/css/pico.pumpkin.min.css" }
+];
 export function Layout({children}: { children: React.ReactNode }) {
     return (
         <html lang="en">
         <head>
             <meta charSet="utf-8"/>
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <title>FilmI</title>
             <Meta/>
             <Links/>
         </head>
