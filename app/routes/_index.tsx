5d01175bdaa8ce4d5095e35dd32ce9926fb7a7e1
Rohit <rohit.verma@itsrizzoli.it>
2024-06-15 10:05:53 +0200

    Initial stage

---
+++ app/routes/_index.tsx
@@ -0,0 +1,16 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "FilmI" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div className="font-sans p-4">
+      <h1 className="text-3xl font-tohama">WE WASH MOVIES</h1>
+    </div>
+  );
+}
