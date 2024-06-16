5a91f3b2c876a3e6d7540fb9a7898dc8e0b335ef
luca <luca.ilari@gmail.com>
2024-06-16 13:07:54 +0200

    Articles

--- app/routes/films.tsx
+++ app/routes/films.tsx
@@ -1,35 +1,41 @@
-import {prisma} from "~/utils/db.server";
-import {json, LoaderFunctionArgs} from "@remix-run/node";
-import {useLoaderData} from "@remix-run/react";
-import {MOVIE} from "~/types/movie-table";
+import { prisma } from "~/utils/db.server";
+import { json, LoaderFunctionArgs } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { MOVIE } from "~/types/movie-table";
 
-
-export const loader = async ({params}: LoaderFunctionArgs) => {
-    try {
-        const movies = await prisma.movie.findMany();
-        console.log("DB has:", movies.length, "movies")
-        return json({movies})
-    } catch (err) {
-        console.log("Error fetching data from DB")
-        return json({err: "Error fetching data from DB"})
-    }
-}
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  try {
+    const movies = await prisma.movie.findMany();
+    console.log("DB has:", movies.length, "movies");
+    return json({ movies });
+  } catch (err) {
+    console.log("Error fetching data from DB");
+    return json({ err: "Error fetching data from DB" });
+  }
+};
 
 function Films() {
-    const {movies} = useLoaderData<{ movies:MOVIE[] }>();
-    return (
-        <div className="text-center">
-            {movies ?
-                <ul>
-                    {movies?.map((movie) => (
-                        <li key={movie?.id}><strong className={"movieName"}>{movie?.name}:</strong> <span style={{fontWeight: 400}}>{movie?.description}</span> </li>
-                    ))}
-                </ul>
-                :
-                <button>No movie found</button>
-            }
-        </div>
-    );
+  const { movies } = useLoaderData<{ movies: MOVIE[] }>();
+  return (
+    <div className="text-center">
+      {movies ? (
+          <>
+          {movies?.map((movie) => (
+              <article title={movie?.name} key={movie?.id}>
+                <header>
+
+                <strong className={"movieName"}>{movie?.name}:</strong>
+                </header>
+                <span style={{ fontWeight: 400 }}>{movie?.description}</span>
+                
+              </article>
+          ))}
+          </>
+      ) : (
+        <button>No movie found</button>
+      )}
+    </div>
+  );
 }
 
 export default Films;
