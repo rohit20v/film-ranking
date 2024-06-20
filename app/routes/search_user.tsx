b9b3426500a105f87dbe0ca7cc7969f54cd32eb4
Luca Ilari <luca.ilari@gmail.com>
2024-06-20 17:53:02 +0200

    TODO: user list page

---
+++ app/routes/search_user.tsx
@@ -0,0 +1,34 @@
+import {ActionFunctionArgs, json} from "@remix-run/node";
+import {Form, redirect, useActionData} from "@remix-run/react";
+import {prisma} from "~/utils/db.server";
+
+export const action = async ({request}: ActionFunctionArgs) => {
+    const formData = await request.formData();
+    const formUsername: string = formData.get("username");
+    if (!formUsername) {
+        return json({err: "Error no username to search"});
+    }
+    const usernameToSearch = formUsername?.toLowerCase();
+    const user = await prisma.user.findFirst({
+        where: {username: usernameToSearch},
+        select: {username: true}
+    })
+    if (user) {
+        return redirect("/user_films/" + usernameToSearch);
+    }
+    return json({err: "Error user not found"})
+}
+
+const Search_user = () => {
+    const userSearcher = useActionData();
+    return (
+        <>
+            <Form method={"POST"}>
+                <input type={"text"} name={"username"} placeholder={"Search your friends!"}/>
+                <button type={"submit"}>Search user</button>
+            </Form>
+        </>
+    )
+}
+
+export default Search_user;
