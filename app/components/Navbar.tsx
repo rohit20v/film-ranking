99fd60a12894b9a1d13b9ef2d256d13563fac0f9
Rohit <rohit.verma@itsrizzoli.it>
2024-06-27 19:55:56 +0200

    Added mobile responsive nav

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,23 @@
 import {NavLink} from "@remix-run/react";
 import ThemeToggler from "~/components/ThemeToggler";
+import {useRef, useState} from "react";
 
 function Navbar() {
+    const menu = useRef()
+    const [isMenuToggled, setIsMenuToggled] = useState(false);
+
+    const toggleMenu = () => {
+        if (menu.current) {
+            menu.current.classList.toggle('ham-menu');
+            setIsMenuToggled(prevState => !prevState)
+        }
+    };
+
     return (
         <nav>
             <ul>
-                <li><a href="./" style={{marginLeft: "8px", textDecoration: "none"}}><strong className={'title'}>CINEPHILIA!</strong></a></li>
+                <li><a href="./" style={{marginLeft: "8px", textDecoration: "none"}}><strong
+                    className={'title'}>CINEPHILIA!</strong></a></li>
             </ul>
             <ul className={"menu-list"}>
                 <li>
@@ -26,14 +38,35 @@ function Navbar() {
                 <li>
                     <ThemeToggler/>
                 </li>
-                <li>
-                    {/*<Lottie*/}
-                    {/*    options={defaultOptions}*/}
-                    {/*    height={400}*/}
-                    {/*    width={400} animationData={hamburgerData}/>*/}
-                    MENU
-                </li>
             </ul>
+            <li className={'ham'}>
+                <span onClick={toggleMenu} ref={menu}>
+                <span hidden={isMenuToggled}>
+                    MENU
+                </span>
+                    {isMenuToggled && (<div className={'ham-menu'}>
+                        <li>
+                            <NavLink className={"navLink"} to={"./"}>Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink className={"navLink"} to={"./films"}>Movies</NavLink>
+                        </li>
+                        <li>
+                            <NavLink className={"navLink"} to={"./search_user"}>Search user</NavLink>
+                        </li>
+                        <li>
+                            <NavLink className={"navLink"} to={"./sign_in"}>Sign In</NavLink>
+                        </li>
+                        <li>
+                            <NavLink className={"navLink"} to={"./login"}>Login</NavLink>
+                        </li>
+                        <li>
+                            <ThemeToggler/>
+                        </li>
+                    </div>)}
+                </span>
+            </li>
+
         </nav>
     );
 }
