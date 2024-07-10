efdb4ceb534de6cceb4af8acef5fb12753e91c4e
Luca Ilari <luca.ilari@gmail.com>
2024-07-10 17:16:04 +0200

    Fix height friends list

--- app/routes/user.$username.tsx
+++ app/routes/user.$username.tsx
@@ -1,18 +1,21 @@
-import { Form, redirect, useLoaderData } from "@remix-run/react";
-import { ActionFunctionArgs, json, LoaderFunctionArgs } from "@remix-run/node";
-import { prisma } from "~/utils/db.server";
-import { getSession } from "~/session";
-import { OnlyStar } from "~/components/Star";
-import { addMoviesTitle } from "~/utils/functions";
+import {Form, redirect, useLoaderData} from "@remix-run/react";
+import {ActionFunctionArgs, json, LoaderFunctionArgs} from "@remix-run/node";
+import {prisma} from "~/utils/db.server";
+import {getSession} from "~/session";
+import {OnlyStar} from "~/components/Star";
+import {addMoviesTitle} from "~/utils/functions";
 import MoviePoster from "~/components/MoviePoster";
 
-export const loader = async ({ request, params }: LoaderFunctionArgs) => {
+export const loader = async ({request, params}: LoaderFunctionArgs) => {
     const session = await getSession(request.headers.get("cookie"));
     const sessionUser = session.data.user;
     let showAddFriendButton = true;
     //TODO: remove friend
     const username = params.username;
-    if (!sessionUser || username === sessionUser /*searched user is the same logged in*/) {
+    if (
+        !sessionUser ||
+        username === sessionUser /*searched user is the same logged in*/
+    ) {
         showAddFriendButton = false;
     }
     const userFriends = await prisma.user.findFirst({
@@ -20,16 +23,18 @@ export const loader = async ({ request, params }: LoaderFunctionArgs) => {
             username: sessionUser,
         },
         select: {
-            following: { include: { friend: { select: { username: true } } } },
+            following: {include: {friend: {select: {username: true}}}},
         },
     });
-    const sessionUserFriends = userFriends?.following.map((x) => x.friend.username);
+    const sessionUserFriends = userFriends?.following.map(
+        (x) => x.friend.username,
+    );
     if (sessionUserFriends?.includes(username)) {
         showAddFriendButton = false;
     }
     const userMovies = await prisma.user.findFirst({
-        where: { username: username },
-        select: { user_movies: true },
+        where: {username: username},
+        select: {user_movies: true},
     });
     if (userMovies === null) {
         return json({
@@ -40,10 +45,10 @@ export const loader = async ({ request, params }: LoaderFunctionArgs) => {
         });
     }
 
-    return json({ err: null, username, userMovies, showAddFriendButton });
+    return json({err: null, username, userMovies, showAddFriendButton});
 };
 
-export const action = async ({ request, params }: ActionFunctionArgs) => {
+export const action = async ({request, params}: ActionFunctionArgs) => {
     const session = await getSession(request.headers.get("cookie"));
     const user = session.data.user;
     if (!user) {
@@ -52,11 +57,11 @@ export const action = async ({ request, params }: ActionFunctionArgs) => {
     }
     const friendToAdd = params.username;
     const friendToAddId = await prisma.user.findFirst({
-        where: { username: friendToAdd },
-        select: { id: true },
+        where: {username: friendToAdd},
+        select: {id: true},
     });
     if (!friendToAddId?.id) {
-        return json({ err: "User not found" });
+        return json({err: "User not found"});
     }
     try {
         const added = await prisma.user.update({
@@ -64,13 +69,13 @@ export const action = async ({ request, params }: ActionFunctionArgs) => {
                 username: user,
             },
             data: {
-                following: { create: { friend_id: friendToAddId.id } },
+                following: {create: {friend_id: friendToAddId.id}},
             },
         });
     } catch (e) {
-        return json({ err: "Error adding friend" });
+        return json({err: "Error adding friend"});
     }
-    return json({ err: null });
+    return json({err: null});
 };
 
 const userFilms = () => {
@@ -86,26 +91,30 @@ const userFilms = () => {
                 <p>user not found</p>
             ) : (
                 <>
-                    <span style={{ fontWeight: "bold", fontSize: 42 }}> {username}</span>
+                    {showAddFriendButton && (
+                        <Form method={"POST"}>
+                            <button type={"submit"}>Add friend</button>
+                        </Form>
+                    )}
+                    <span style={{fontWeight: "bold", fontSize: 42}}> {username}</span>
                     {userMovies?.length > 0 ? (
                         <>
                             {userMovies?.map((movie) => (
-                                <article title={movie?.title} key={movie?.id}>
+                                <article
+                                    style={{maxWidth: "500px"}}
+                                    title={movie?.title}
+                                    key={movie?.id}
+                                >
                                     <header>
                                         <strong className={"movieName"}>{movie?.title}</strong>
                                     </header>
-                                    <MoviePoster name={movie?.title} tconst={movie?.tconst} />
-                                    <OnlyStar star={parseInt(movie?.rating)} />
+                                    <MoviePoster name={movie?.title} tconst={movie?.tconst}/>
+                                    <OnlyStar star={parseInt(movie?.rating)}/>
                                 </article>
                             ))}
                         </>
                     ) : (
                         <p>No movie found</p>
-                    )}
-                    {showAddFriendButton && (
-                        <Form method={"POST"}>
-                            <button type={"submit"}>Add friend</button>
-                        </Form>
                     )}
                 </>
             )}
